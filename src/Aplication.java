44754b7fcb4ca88a668f291d9cbbbc91ee7ccfc0
Mansur <up202102355@fe.up.pt>
2022-09-24 18:35:06 +0100

    Added a welcoming message

--- src/Aplication.java
+++ src/Aplication.java
@@ -1,6 +1,6 @@
 public class Aplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("\"Thank you for using the Hello World App\"");
     }
 }
 

b2b2677ed3fa00aa8aa9842f64e555e8dc67f1f6
Mansur <up202102355@fe.up.pt>
2022-09-27 11:44:15 +0100

    Chengs

--- src/main/java/com/mansur/hero/Main.java
+++ src/main/java/com/mansur/hero/Main.java
@@ -2,6 +2,6 @@ package com.mansur.hero;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! 2");
+        System.out.println("Hello world!");
     }
 }

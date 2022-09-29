9c7995ea14b55f4f4761d799ed761acf552442c4
Mansur <up202102355@fe.up.pt>
2022-09-29 22:40:17 +0100

    Step 7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,9 +11,10 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game() throws IOException {
-
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
             DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
@@ -23,18 +26,33 @@ public class Game {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    private void draw() throws IOException {
+
+    private void processKey(KeyStroke key) {
+        if(key.getKeyType() == KeyType.ArrowUp)  y -= 1;
+        if(key.getKeyType() == KeyType.ArrowDown)  y += 1;
+        if(key.getKeyType() == KeyType.ArrowLeft)  x -= 1;
+        if(key.getKeyType() == KeyType.ArrowRight)  x += 1;
+    }
+
+    private void draw(int x , int y ) throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-                [0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() throws IOException {
-        draw();
+        while(true){
+            draw(x,y);
+            KeyStroke key = screen.readInput();
+            if(key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+                screen.close();
+            }
+            if(key.getKeyType() == KeyType.EOF) break;
+            processKey(key);
+        }
+
     }
 
 

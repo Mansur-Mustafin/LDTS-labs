3e1285c56fcf234b5c8a7ac683d06d9f84417251
Mansur <up202102355@fe.up.pt>
2022-09-29 21:31:41 +0100

    Step 5

--- src/main/java/com/mansur/hero/Main.java
+++ src/main/java/com/mansur/hero/Main.java
@@ -1,7 +1,40 @@
 package com.mansur.hero;
 
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Screen screen = null;
+        try {
+            Terminal terminal = new
+                    DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new
+                DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
+                [0]);
+        screen.refresh();
+
     }
 }

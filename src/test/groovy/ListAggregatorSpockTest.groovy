0b4455f8f45fab21756d84b9e0767ded76b8a8f2
Mansur <up202102355@fe.up.pt>
2022-11-03 19:19:19 +0000

    List filter

--- src/test/groovy/ListAggregatorSpockTest.groovy
+++ src/test/groovy/ListAggregatorSpockTest.groovy
@@ -54,7 +54,7 @@ class ListAggregatorSpockTest extends Specification{
     def 'testing distinct bug 8726'(){
         given:
             def deduplicator = Mock(GenericListDeduplicator)
-        deduplicator.deduplicate(_) >>> [Arrays.asList(1, 2, 4), Arrays.asList(6, 7)]
+            deduplicator.deduplicate(_) >>> [Arrays.asList(1, 2, 4), Arrays.asList(6, 7)]
         when:
             def result = deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2))
         then:
--- src/test/groovy/ListFilterSpockTest.groovy
+++ src/test/groovy/ListFilterSpockTest.groovy
@@ -1,5 +1,23 @@
+import com.aor.numbers.GenericListFilter
+import com.aor.numbers.ListFilterer
 import spock.lang.Specification
 
 class ListFilterSpockTest extends Specification{
+    private def list
+
+    def setup() {
+        list = Arrays.asList(1,-2,-4,2,5)
+    }
+
+    def 'test list filter'(){
+        given:
+        def filter = Mock(GenericListFilter)
+        filter.accept(_) >>> [true, false, false, true, true]
+        def f = new ListFilterer(filter)
+        when:
+        def r = f.filter(list)
+        then:
+        r == [1,2,5]
+    }
 
 }

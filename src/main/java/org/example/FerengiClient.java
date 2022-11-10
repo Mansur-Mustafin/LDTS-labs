f853d1641782d642e4a7a85863a229cf8f7526a9
Mansur <up202102355@fe.up.pt>
2022-11-10 02:28:38 +0000

    Step 5 Done

---
+++ src/main/java/org/example/FerengiClient.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class FerengiClient extends AlienClient{
+    @Override
+    protected OrderingStrategy createOrderingStrategy(){
+        return new SmartStrategy();
+    }
+}
